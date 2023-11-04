0b99b8b4c9d951e21432d251454c274cb6ed359e
Quincy <quincy_lim@protonmail.com>
2023-11-04 22:14:33 +0800

    add preprocess civit ai script

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # kcg-ml-n-grams  
 
 ## Simple Prompt Generator  
+Preprocess the civitai csv file first
+```
+$ python scripts/preprocess_civitai_csv.py \
+    --data_csv_path <civitai csv file with phrases>
+    --output_path <output path to save processed csv file>
+```
+
 ```
 $ python scripts/generate_prompts.py  \
-    --data_csv_path <civitai csv file with phrases. must have probability and log probability columns> \
+    --data_csv_path <processed civitai csv file. must have probability and log probability columns> \
     --n_prompt <number of prompts to generate> \
     --save_path <msgpack save path>
 ```
---
+++ scripts/preprocess_civitai_csv.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+
+import argparse
+
+def filter_civitai(df):
+    # Remove rows with unicode characters
+    df = df[df['phrase str'].apply(lambda x: isinstance(x, str) and all(ord(char) < 128 for char in x))]
+
+    # Remove rows with tab
+    df = df[df['phrase str'].str.contains('\t') == False]
+
+    # Remove rows with more than 100 characters
+    df = df[df['phrase str'].str.len() <= 100]
+
+    return df
+
+def add_features(df):
+    """Adds probability to dataframe and log probability based on positive counts
+
+    Args:
+        df (pd.DataFrame): civitai csv Dataframe
+    """
+    df['probability'] = df['positive count'] / df['positive count'].sum()
+    df['log probability'] = np.log(df['probability'] + 1e-15)
+
+    return df
+
+def main(df):
+    df = filter_civitai(df)
+    df = add_features(df)
+
+    return df
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--data_csv_path', type=str, help='Path to civitai csv file')
+    ap.add_argument('--output_path', type=str, help='Path to save processed csv')
+    args = ap.parse_args()
+
+    df = pd.read_csv(args.data_csv_path)
+    df = main(df)
+    df.to_csv(args.output_path, index=False)
+
