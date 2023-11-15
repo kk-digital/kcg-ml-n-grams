323d09130686665caf4ca862cc4725d60ac6df29
Quincy <quincy_lim@protonmail.com>
2023-11-15 14:42:02 +0800

    update extract n-gram script

--- scripts/extract_environmental_ngrams.py
+++ scripts/extract_environmental_ngrams.py
@@ -29,6 +29,8 @@ ngram_constraints = {
 }
 
 def create_ngrams(vocab, n):
+    if isinstance(vocab, float):
+        return []
     ngram_data = []
     max_index = len(vocab) - n + 1
 
@@ -45,14 +47,19 @@ def get_token_length(row):
     with torch.no_grad():
         token_encoding = tokenizer(row['phrase str'], return_length=True, return_tensors='pt')
 
-    return token_encoding['length'].item()
+    # subtract 2 to remove <start> <end> token counts
+    return token_encoding['length'].item() - 2
 
 def filter_phrase(df, max_phrase_length=150, max_token_length=20):
     # Remove rows with unicode characters
     df = df[df['phrase str'].apply(lambda x: isinstance(x, str) and all(ord(char) < 128 for char in x))]
 
     # Remove rows with tab
-    df = df[df['phrase str'].str.contains('\t|\n|\r|\b') == False]
+    # df = df[df['phrase str'].str.contains('\t|\n|\r|\b') == False]
+    df = df[~df['phrase str'].str.contains('|'.join(['\t', '\n', '\r', '\b']))]
+
+    # Remove special characters
+    df = df[~df['phrase str'].str.contains(':|&')]
 
     # Remove rows with more than 100 characters
     df = df[(df['phrase str'].str.len() <= max_phrase_length) & (df['phrase str'].str.len() > 0)]
@@ -74,46 +81,64 @@ def count_positive_negative_phrase(df, ngram):
 
     phrases = positive_tokens_chain + negative_tokens_chain
     phrases = list(set(phrases))
+    # remove nan
+    phrases = [p for p in phrases if isinstance(p, str)]
+    print(f'Phrase: {len(phrases)}')
 
     positive_phrase_counts = pd.DataFrame(Counter(positive_tokens_chain), index=['positive count']).T
     negative_phrase_counts = pd.DataFrame(Counter(negative_tokens_chain), index=['negative count']).T
+    print(f'Positive phrae: {len(positive_phrase_counts)}')
 
     df_phrase_counts = positive_phrase_counts.merge(negative_phrase_counts, how='outer', left_index=True, right_index=True)
     df_phrase_counts = df_phrase_counts.fillna(0).astype(np.int32)
     df_phrase_counts['phrase str'] = df_phrase_counts.index
     df_phrase_counts['token_length'] = df_phrase_counts.apply(get_token_length, axis=1)
     df_phrase_counts = df_phrase_counts.reset_index(drop=True)
+    print(f'Phrase count: {len(df_phrase_counts)}')
 
     df_phrase_counts = filter_phrase(df_phrase_counts)
+    print(f'Phrase count: {len(df_phrase_counts)}')
+
     df_phrase_counts = df_phrase_counts[['phrase str', 'token_length', 'positive count', 'negative count']]
 
     return df_phrase_counts
 
-def main(data_path, ngram, save_path):
-    file_paths = sorted(glob.glob(os.path.join(data_path, '*_embedding.msgpack')))
-    df = []
-    for file_path in tqdm.tqdm(file_paths):
-        data = read_msg_pack(file_path)
-        del data['positive_embedding']
-        del data['negative_embedding']
-        df.append(data)
+def main(
+    ngram,
+    save_path,
+    data_path=None,
+    data_csv_path=None
+):
+    if data_csv_path:
+        df = pd.read_csv(data_csv_path)
+    else:
+        file_paths = sorted(glob.glob(os.path.join(data_path, '*_embedding.msgpack')))
+        df = []
+        for file_path in tqdm.tqdm(file_paths):
+            data = read_msg_pack(file_path)
+            del data['positive_embedding']
+            del data['negative_embedding']
+            df.append(data)
 
-    df = pd.DataFrame(df)
+        df = pd.DataFrame(df)
+        df.to_csv(os.path.join(save_path, 'environment_data.csv'), index=False)
+
     df_phrase_counts = count_positive_negative_phrase(df, ngram)
 
     os.makedirs(save_path, exist_ok=True)
-    df.to_csv(os.path.join(save_path, 'environment_data.csv'), index=False)
     df_phrase_counts.to_csv(os.path.join(save_path, f'environment_{ngram}-gram.csv'), index=False)
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument('--data_path', type=str, help='Path to environment embedding msgpack')
+    ap.add_argument('--data_csv_path', type=str, help='Path to csv with extracted data from msgpack, data_path is omitted if this is passed in')
     ap.add_argument('--ngram', type=int, help='ngram to extract')
     ap.add_argument('--save_path', type=str, help='Path to save output files')
     args = ap.parse_args()
 
     main(
         data_path=args.data_path,
+        data_csv_path=args.data_csv_path,
         ngram=args.ngram,
         save_path=args.save_path
     )
